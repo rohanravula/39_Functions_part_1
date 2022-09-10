01feec2b24dc78170acd8e183689dec870a234e9
rohanravula <109545469+rohanravula@users.noreply.github.com>
2022-09-10 23:50:03 +0530

    Add files via upload

---
+++ 1_functions.py
@@ -0,0 +1,66 @@
+"1"
+# def rohan():
+#     print("hello world")
+# rohan() #hello world
+
+# def rohan():
+#     print("rohan")
+#     rohan()
+# rohan() #recursion error
+
+"2"
+# def rohan(n):
+#     print("hello "+n)
+# rohan("R.k")
+# rohan("kumar")
+# rohan("5")
+
+"3"
+# def roh(n,g):
+#     print("hello "+n,g)
+# roh("rohan","kumar")
+# roh("mohan", "goud")
+
+# def roh(n,g):
+#     print("hello "+g)
+# roh("mohan","rohan") 
+
+"4"
+# def roh(*names):
+#     print("hello",names)
+# roh("python","rohan","kumar","bye")
+
+# def roh(*names):
+#     print("hello",names[3])
+# roh("python","rohan","kumar","bye")
+
+"5"
+# def detail(n,a):
+#     print("hello "+n+"! your age is"+str(a))
+# detail("rohan",20)
+# detail(n="rohan",a=20)
+# detail(a=20,n="rohan")
+
+"6"
+
+# def detail(n,a):
+#     print("hello "+n +" your age" +str(a))
+# n=input("enter your name: ")
+# a=input("enter your age: ")
+# detail(n,a)
+
+"7"
+# def r(x,y):
+#     return(x*y)
+# print(r(5,6))
+"or"
+# def r(x,y):
+#     return print(x*y)
+# r(10,20)
+
+"8"
+def z(p,q):
+    return p*q,p+q
+p=int(input())
+q=int(input())
+print(z(p,q)) 
